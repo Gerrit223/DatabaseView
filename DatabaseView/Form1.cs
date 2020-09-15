6706aba668d850b1ae8d93dd8cf1a822e9f12e31
Gerrit223 <71283170+Gerrit223@users.noreply.github.com>
2020-09-15 14:02:52 +0200

    "Second commit"

--- DatabaseView/Form1.cs
+++ DatabaseView/Form1.cs
@@ -20,6 +20,7 @@ namespace DatabaseView
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Hello)");
+            MessageBox.Show("Second commit");
         }
     }
 }
